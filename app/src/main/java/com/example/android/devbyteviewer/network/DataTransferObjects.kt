3a8a43a8beef3ec72fba324827bfcd68d7514048
polojerry <polojerry4@gmail.com>
2020-03-19 14:32:45 +0300

    Added an Extension function to convert Network Video to Database Video

--- app/src/main/java/com/example/android/devbyteviewer/network/DataTransferObjects.kt
+++ app/src/main/java/com/example/android/devbyteviewer/network/DataTransferObjects.kt
@@ -17,6 +17,7 @@
 
 package com.example.android.devbyteviewer.network
 
+import com.example.android.devbyteviewer.database.DatabaseVideo
 import com.example.android.devbyteviewer.domain.Video
 import com.squareup.moshi.JsonClass
 
@@ -26,8 +27,6 @@ import com.squareup.moshi.JsonClass
  * using them.
  */
 
-// TODO (03) Define extension function NetworkVideoHolder.asDatabaseModel(),
-// that returns an array of <DatabaseVideo>.
 
 /**
  * VideoHolder holds a list of Videos.
@@ -65,4 +64,16 @@ fun NetworkVideoContainer.asDomainModel(): List<Video> {
                 updated = it.updated,
                 thumbnail = it.thumbnail)
     }
+}
+
+fun NetworkVideoContainer.asDatabaseModel() : Array<DatabaseVideo>{
+    return videos.map {
+        DatabaseVideo(
+                url = it.url,
+                updated = it.updated,
+                title = it.updated,
+                description = it.description,
+                thumbnail = it.thumbnail
+        )
+    }.toTypedArray()
 }

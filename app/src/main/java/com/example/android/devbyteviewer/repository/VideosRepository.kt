869896b1ed22fcc5bb7750b127cb54dcd2157c81
polojerry <polojerry4@gmail.com>
2020-03-19 15:38:57 +0300

    Created a repository.tranformation map. to convert database video to VIdeo

--- app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
+++ app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -17,7 +17,11 @@
 
 package com.example.android.devbyteviewer.repository
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
 import com.example.android.devbyteviewer.database.VideosDatabase
+import com.example.android.devbyteviewer.database.asDomainModel
+import com.example.android.devbyteviewer.domain.Video
 import com.example.android.devbyteviewer.network.Network
 import com.example.android.devbyteviewer.network.asDatabaseModel
 import kotlinx.coroutines.Dispatchers
@@ -30,5 +34,13 @@ class VideosRepository(val database : VideosDatabase){
             database.videoDao.insertAll(*playlist.asDatabaseModel())
         }
     }
+
+    //TO be used by IO to diplay VIdeo
+    val videos: LiveData<List<Video>> =
+            Transformations.map(database.videoDao.getVideos()) {
+                it.asDomainModel()
+            }
 }
+
+
 
